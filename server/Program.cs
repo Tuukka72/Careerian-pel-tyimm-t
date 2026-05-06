8b22a87fc60a70d406284c6013a510ab37d090b4
Anton Schroderus <Coffee.overdoser@gmail.com>
2026-05-06 10:40:38 +0300

    Tietokanta luotu ja yhdistetty, siinä on get ja post.

--- server/Program.cs
+++ server/Program.cs
@@ -1,3 +1,5 @@
+using Npgsql;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -26,4 +28,56 @@ app.UseHttpsRedirection();
 
 app.MapGet("/api/test", () => "Hello from backend!");
 
+app.MapGet("/kyydit", async (IConfiguration config) =>
+{
+    var connString = config.GetConnectionString("DefaultConnection");
+
+    var kyydit = new List<object>();
+
+    await using var conn = new NpgsqlConnection(connString);
+    await conn.OpenAsync();
+
+    var cmd = new NpgsqlCommand("SELECT * FROM kyydit", conn);
+    var reader = await cmd.ExecuteReaderAsync();
+
+    while (await reader.ReadAsync())
+    {
+        kyydit.Add(new
+        {
+            id = reader.GetInt32(0),
+            kuski_id = reader.GetInt32(1),
+            mista = reader.GetString(2),
+            mihin = reader.GetString(3),
+            lahtoaika = reader.GetDateTime(4),
+            paikkoja = reader.GetInt32(5)
+        });
+    }
+
+    return kyydit;
+});
+app.MapPost("/kyydit", async (IConfiguration config, Kyyti uusi) =>
+{
+    var connString = config.GetConnectionString("DefaultConnection");
+
+    await using var conn = new NpgsqlConnection(connString);
+    await conn.OpenAsync();
+
+    var cmd = new NpgsqlCommand(
+        "INSERT INTO kyydit (kuski_id, mista_paikasta, mihin_paikkaan, lahtoaika, paikkoja_saatavilla) VALUES ($1, $2, $3, $4, $5)",
+        conn
+    );
+
+    cmd.Parameters.AddWithValue(uusi.kuski_id);
+    cmd.Parameters.AddWithValue(uusi.mista);
+    cmd.Parameters.AddWithValue(uusi.mihin);
+    cmd.Parameters.AddWithValue(uusi.lahtoaika);
+    cmd.Parameters.AddWithValue(uusi.paikkoja);
+
+    await cmd.ExecuteNonQueryAsync();
+
+    return Results.Ok();
+});
+
 app.Run();
+
+record Kyyti(int kuski_id, string mista, string mihin, DateTime lahtoaika, int paikkoja);
