4ec51d980a44ced7056dbcbfc56af9818655f2dd
tuukka <tukkaz964@gmail.com>
2026-05-26 09:39:26 +0300

    Revert "should work mayby"
    This reverts commit 5cb3f088b7ef19bcd56cba0315b89bb93c6287fc.

--- server/obj/Debug/net10.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net10.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efdc11ea934c7e64c547d13e62a1a0331ddaf556")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c27d54789687a8020ec73a6af616fb58f6ff9203")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
